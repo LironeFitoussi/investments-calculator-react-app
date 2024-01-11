06dbb6cd23fd217e6f0e96730434f1c439277026
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-01-11 19:31:37 +0200

    Output base table set

---
+++ src/components/Results/Results.jsx
@@ -0,0 +1,16 @@
+import './Results.css'
+export default function Results({ initialInvestment, annualInvestment, expectedReturn, duration }) {
+    console.log(initialInvestment);
+    return (
+        <table id='result'>
+            <thead>
+                <tr></tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    )
+};
